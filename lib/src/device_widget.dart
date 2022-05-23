c3ee8972d6602eaec9b32c6507c0525174db149c
smartwalle <smartwalle@gmail.com>
2022-05-23 17:47:54 +0800

    update

--- lib/src/device_widget.dart
+++ lib/src/device_widget.dart
@@ -1,32 +1,32 @@
 import 'package:flutter/widgets.dart';
 import 'package:sm_platform/src/device.dart';
 
-class DeviceLayout extends StatelessWidget {
-  final Widget phone;
-  final Widget tablet;
-  final Widget desktop;
-  final Widget browser;
+class DeviceWidget extends StatelessWidget {
+  final Widget? phone;
+  final Widget? tablet;
+  final Widget? desktop;
+  final Widget? browser;
   final Widget child;
 
-  DeviceLayout({
-    Key key,
+  DeviceWidget({
+    Key? key,
     this.phone,
     this.tablet,
     this.desktop,
     this.browser,
-    this.child,
+    required this.child,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     if (Device.isBrowser && this.browser != null) {
-      return this.browser;
+      return this.browser!;
     } else if (Device.isDesktop && this.desktop != null) {
-      return this.desktop;
+      return this.desktop!;
     } else if (Device.isTablet && this.tablet != null) {
-      return this.tablet;
+      return this.tablet!;
     } else if (Device.isPhone && this.phone != null) {
-      return this.phone;
+      return this.phone!;
     }
     return this.child;
   }
--- lib/src/platform_widget.dart
+++ lib/src/platform_widget.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/widgets.dart';
 import 'package:sm_platform/src/platform.dart';
 
-class PlatformLayout extends StatelessWidget {
-  final Widget web;
-  final Widget android;
-  final Widget iOS;
-  final Widget macOS;
-  final Widget windows;
-  final Widget fuchsia;
-  final Widget linux;
+class PlatformWidget extends StatelessWidget {
+  final Widget? web;
+  final Widget? android;
+  final Widget? iOS;
+  final Widget? macOS;
+  final Widget? windows;
+  final Widget? fuchsia;
+  final Widget? linux;
   final Widget child;
 
-  PlatformLayout({
-    Key key,
+  PlatformWidget({
+    Key? key,
     this.web,
     this.android,
     this.iOS,
@@ -20,26 +20,25 @@ class PlatformLayout extends StatelessWidget {
     this.windows,
     this.fuchsia,
     this.linux,
-    this.child,
-  })  : assert(child != null),
-        super(key: key);
+    required this.child,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     if (Platform.isWeb && this.web != null) {
-      return this.web;
+      return this.web!;
     } else if (Platform.isAndroid && this.android != null) {
-      return this.android;
+      return this.android!;
     } else if (Platform.isIOS && this.iOS != null) {
-      return this.iOS;
+      return this.iOS!;
     } else if (Platform.isMacOS && this.macOS != null) {
-      return this.macOS;
+      return this.macOS!;
     } else if (Platform.isWindows && this.windows != null) {
-      return this.windows;
+      return this.windows!;
     } else if (Platform.isFuchsia && this.fuchsia != null) {
-      return this.fuchsia;
+      return this.fuchsia!;
     } else if (Platform.isLinux && this.linux != null) {
-      return this.linux;
+      return this.linux!;
     }
     return this.child;
   }
